5bb0e98fe570f9f7543ac9498cf6b7f6f2ec8d12
81Jeremiah <jeremiahlutes@gmail.com>
2019-06-18 21:39:38 -0700

    user account page component

---
+++ src/containers/Account.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default class Account extends Component {
+
+  render(){
+    return(
+      <div>
+      User Account
+      </div>
+    );
+  }
+
+
+}
