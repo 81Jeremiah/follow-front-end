46cb54f7b80f55c1aa39f6af2bc577e95916e172
81Jeremiah <jeremiahlutes@gmail.com>
2019-06-19 09:14:58 -0700

    fixed basic css labels for react

--- src/Login.js
+++ src/Login.js
@@ -5,20 +5,20 @@ export default class Login extends Component {
 
 render() {
   return(
-    <form class="form-signin">
-         <img class="mb-4" src="" alt="" width="72" height="72"/>
-         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-         <label for="inputEmail" class="sr-only">Email address</label>
-         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus/>
-         <label for="inputPassword" class="sr-only">Password</label>
-         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/>
-         <div class="checkbox mb-3">
+    <form className="form-signin">
+         <img className="mb-4" src="" alt="" width="72" height="72"/>
+         <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+         <label htmlFor="inputEmail" className="sr-only">Email address</label>
+         <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus/>
+         <label htmlFor="inputPassword" className="sr-only">Password</label>
+         <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+         <div className="checkbox mb-3">
            <label>
              <input type="checkbox" value="remember-me"/> Remember me
            </label>
          </div>
-         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-         <p class="mt-5 mb-3 text-muted"> </p>
+         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+         <p className="mt-5 mb-3 text-muted"> </p>
        </form>
   );
  }
