28d50932f4322239aa8ed7c2aa3dd32d33eee81a
81Jeremiah <jeremiahlutes@gmail.com>
2019-06-21 08:27:10 -0700

    implemented restful routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from './Login'
+import Routes from './routes/Routes'
 
 function App() {
   return (
     <div className="App">
      <Login />
+     <Routes />
     </div>
   );
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Account from '../container/Routes'
+import Account from '../containers/Account';
+import Settings from '../Components/Settings';
+import Login from '../Login';
 
 const Routes = props => {
 
   return (
     <Router>
       <Switch>
-        <Route exact path="/account" component={ Account}/>
+        <Route exact path="/account" component={ Account }/>
+        <Route exact path="/settings" component={ Settings }/>
+        <Route exact path="/login" component={ Login}/>
       </Switch>
     </Router>
   )
