313f7081dbebbfd0417df98b027b00eb046f37fc
81Jeremiah <jeremiahlutes@gmail.com>
2019-06-25 21:21:26 -0700

    restored settings page

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import demoScreen from '../img/demo-screen-1.jpg';
+import appBadge from '../img/app-store-badge.svg';
+import googleBadge from '../img/google-play-badge.svg'
+import { Link } from 'react-router-dom';
 
 const Splash = () =>{
 
@@ -9,7 +13,7 @@ const Splash = () =>{
 <div clasName="wrapper">
   <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div className="container">
-      <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <Link to="/signup" className="navbar-brand js-scroll-trigger">Create Account</Link>
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i className="fas fa-bars"></i>
@@ -17,7 +21,7 @@ const Splash = () =>{
       <div className="collapse navbar-collapse" id="navbarResponsive">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#download">Download</a>
+           <Link to="/login" className="nav-link js-scroll-trigger" >Login</Link>
           </li>
           <li className="nav-item">
             <a className="nav-link js-scroll-trigger" href="#features">Features</a>
@@ -45,7 +49,7 @@ const Splash = () =>{
             <div className="device">
               <div className="screen">
                  {/* Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! */}
-                <img src="../img/demo-screen-1.jpg" className="img-fluid" alt=""/>
+                <img src={ demoScreen } className="img-fluid" alt=""/>
               </div>
               <div className="button">
                 {/* You can hook the "home button" to some JavaScript events or just remove it*/}
@@ -65,8 +69,8 @@ const Splash = () =>{
         <h2 className="section-heading">Discover what all the buzz is about!</h2>
         <p>Our app is available on any mobile device! Download now to get started!</p>
         <div className="badges">
-          <a className="badge-link" href="page-top"><img src="../img/google-play-badge.svg" alt=""/></a>
-          <a className="badge-link" href="page-top"><img src="../img/app-store-badge.svg" alt=""/></a>
+          <a className="badge-link" href="page-top"><img src={appBadge} alt="app store"/></a>
+          <a className="badge-link" href="page-top"><img src={googleBadge} alt="google play"/></a>
         </div>
       </div>
     </div>
@@ -87,7 +91,7 @@ const Splash = () =>{
             <div className="device">
               <div className="screen">
               {/*Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! */}
-                <img src="../img/demo-screen-1.jpg" className="img-fluid" alt=""/>
+                <img src={ demoScreen } className="img-fluid" alt=""/>
               </div>
               <div className="button">
                  {/* You can hook the "home button" to some JavaScript events or just remove it*/}
