b9892b86c7e2ff16c6bbb376595d173665705265
81Jeremiah <jeremiahlutes@gmail.com>
2019-07-14 15:56:40 -0700

    added connect and brought in dispatch actions

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,67 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import { login } from './actions/Auth'
+import { connect } from 'react-redux';
 
+class Login extends Component {
 
-export default class Login extends Component {
+  constructor(props) {
+  super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  // state = {
+  //   email: "",
+  //   password: "",
+  // };
+
+handleChange = (event) => {
+
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    // const  user = new FormData();
+    // user.append('[user]email', this.state.email)
+    // user.append('[user]password', this.state.password)
+    // console.log(user)
+    this.props.login(this.state)
+
+  }
 
 render() {
   return(
-    <form className="form-signin">
-         <img className="mb-4" src="" alt="" width="72" height="72"/>
+    <form className="form-signin" onSubmit={ this.handleSubmit }>
+         {/* <img className="mb-4" src="" alt="" width="72" height="72"/> */}
          <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-         <label htmlFor="inputEmail" className="sr-only">Email address</label>
-         <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus/>
-         <label htmlFor="inputPassword" className="sr-only">Password</label>
-         <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
-         <div className="checkbox mb-3">
+
+      {/* <label htmlFor="inputEmail" className="sr-only">Email address</label> */}
+
+       <input type="email" id="inputEmail"
+                className="form-control"
+                placeholder="Email address"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                required autoFocus />
+
+     {/* <label htmlFor="inputPassword" className="sr-only">Password</label> */}
+
+        <input type="password" id="inputPassword"
+                className="form-control"
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                required />
+
+       <div className="checkbox mb-3">
            <label>
              <input type="checkbox" value="remember-me"/> Remember me
            </label>
@@ -23,3 +72,5 @@ render() {
   );
  }
 }
+
+export default connect(null, { login }) (Login);
